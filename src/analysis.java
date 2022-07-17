34fe09514134843e8cdf40460363bd2bda6e8e32
Jayed Rafi <jayedrafi@ieee.org>
2022-07-17 00:09:28 -0500

    analysis demo
    not comp yet

--- src/analysis.java
+++ src/analysis.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
 public class analysis {
+    private dbAccessor dbAccess;
+    public analysis(){
+        dbAccess=new dbAccessor();
+    }
+
+    public void analyzeByCustomer(String id){
+        ArrayList<String[]>localData = new ArrayList<String[]>();
+        for(int i=0; i<dbAccess.getColG().size(); i++){
+            if(dbAccess.getColG().get(i).compareToIgnoreCase(id)==0){
+                localData.add(dbAccess.getRow(i));
+            }
+        }
+
+        //summery
+
+        //comparison
+
+        //return
+
+    }
 }
