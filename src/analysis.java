bd851629969a753649333ed8a515b812d36f330b
Jayed Rafi <jayedrafi@ieee.org>
2022-07-22 02:20:25 -0500

    Update analysis.java

--- src/analysis.java
+++ src/analysis.java
@@ -24,15 +24,36 @@ public class analysis {
         String[]data={"NA","NA","NA","NA","NA"};
         for(int i=0; i<customerSummary.size(); i++){
             if(customerSummary.get(i)[0].compareToIgnoreCase(id)==0){
-
+                data=customerSummary.get(i);//replace
             }
         }
         return data;
     }
 
     //Process summary of customers
+    //First store all ids
+    //Format: [id],[location],[total quantity fall,winter,spring,summer],[money spent fall,winter,spring,summer]
     void summaryProcessor(){
+        ArrayList<String>id = new ArrayList<String>();
+        dbAccessor dbObj = new dbAccessor();
+        ArrayList<String> colA = dbObj.getColA();
 
+        boolean condition = true;
+        //Make a list of all unique ids
+        //1 because first row is not valid data
+        for(int i=1;i<colA.size(); i++){
+            for(int j=0; j<id.size();j++){
+                if(colA.get(i).compareTo(id.get(j))==0){
+                    condition=false;
+                }
+                if(condition){
+                    id.add(colA.get(i));
+                    condition=true;
+                }
+            }
+        }
+
+        
     }
 
     //helper method to generate summary
