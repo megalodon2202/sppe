0da51198a364c54e88e1a8aab63f18f77f750622
Jayed Rafi <jayedrafi@ieee.org>
2022-07-22 02:36:05 -0500

    date constructors

--- src/console.java
+++ src/console.java
@@ -1,15 +1,16 @@
 import java.util.ArrayList;
 public class console {
     public static void main(String[]args){
-        System.out.println("Testing...");
+        //System.out.println("Testing...");
         metric info = new metric();
-        dbAccessor t = new dbAccessor();
-        System.out.println(t.getRow(0)[0]);
-        info.toString(info.searchByCountry("france"));
-        System.out.println("Total result: "+info.searchByCountry("france").size());
-        filter f = new filter();
-        System.out.println(f.getApproval());
-        info.toString(info.searchByProduct("85123A"));
+        //dbAccessor t = new dbAccessor();
+        //System.out.println(t.getRow(0)[0]);
+        //info.toString(info.searchByCountry("france"));
+        //System.out.println("Total result: "+info.searchByCountry("france").size());
+        //filter f = new filter();
+        //System.out.println(f.getApproval());
+        //info.toString(info.searchByProduct("85123A"));
+        info.toString(info.searchByCustomer("17850"));
     }
 
 
--- src/tool.java
+++ src/tool.java
@@ -1,2 +1,17 @@
 public class tool {
+    public tool(){
+
+    }
+
+    public int getDate(String ufs){
+        return 0;
+    }
+
+    public int getMonth(String ufs){
+        return 0;
+    }
+
+    public int getYear(String ufs){
+        return 0;
+    }
 }
