5a070c42c48562c38a4f03b8b1fa20f668f4d868
Jayed Rafi <jayedrafi@ieee.org>
2022-07-23 23:26:47 -0500

    comment on src/tool.java

--- src/tool.java
+++ src/tool.java
@@ -11,24 +11,28 @@ public class tool {
         return Integer.parseInt(_date[1]);
     }
 
+    //format: MM/DD/YY HH:MM
     public int getMonth(String ufs){
         String[] _time = ufs.split(" ");
         String[]_date=_time[0].split("/");
         return Integer.parseInt(_date[0]);
     }
 
+    //format: MM/DD/YY HH:MM
     public int getYear(String ufs){
         String[] _time = ufs.split(" ");
         String[]_date=_time[0].split("/");
         return Integer.parseInt(_date[2]);
     }
 
+    //format: MM/DD/YY HH:MM
     public int getHour(String ufs){
         String[] _time = ufs.split(" ");
         String[]_date=_time[1].split(":");
         return Integer.parseInt(_date[0]);
     }
 
+    //format: MM/DD/YY HH:MM
     public int getMin(String ufs){
         String[] _time = ufs.split(" ");
         String[]_date=_time[1].split(":");
