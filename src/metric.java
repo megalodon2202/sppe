c1ea58d72882bd7095cb1957625e79e0e2a4a240
Jashann <jashangill3592@gmail.com>
2022-07-25 12:48:34 -0500

    Adding search by description

--- src/metric.java
+++ src/metric.java
@@ -93,8 +93,39 @@ public class metric {
     //user input can be one word or multiple
     public ArrayList<String[]> searchByDescription(String info){
         ArrayList<String[]>data=new ArrayList<String[]>();
-        //jashan
+
+        if ( info.trim().length() > 0 ) {
+            // gets all the descriptions
+            ArrayList<String> colC = dbAccess.getColC();
+
+            for (int i = 0; i < colC.size(); i++) {
+                if (containsIgnoreCase(colC.get(i), info))
+                    data.add(dbAccess.getRow(i));
+            }
+        }
+
         return data;
+    }
+
+    private boolean containsIgnoreCase(String src, String what) {
+        final int length = what.length();
+        if (length == 0)
+            return true; // Empty string is contained
+
+        final char firstLo = Character.toLowerCase(what.charAt(0));
+        final char firstUp = Character.toUpperCase(what.charAt(0));
+
+        for (int i = src.length() - length; i >= 0; i--) {
+            // Quick check before calling the more expensive regionMatches() method:
+            final char ch = src.charAt(i);
+            if (ch != firstLo && ch != firstUp)
+                continue;
+
+            if (src.regionMatches(true, i, what, 0, length))
+                return true;
+        }
+
+        return false;
     }
 
     //metric: request maintainer for the user interface
