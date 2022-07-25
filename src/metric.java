7029ed255464da559694a8358e2ff3cf65cbb629
Jashann <jashangill3592@gmail.com>
2022-07-25 12:22:47 -0500

    Fixing formatting

--- src/metric.java
+++ src/metric.java
@@ -93,7 +93,7 @@ public class metric {
     //user input can be one word or multiple
     public ArrayList<String[]> searchByDescription(String info){
         ArrayList<String[]>data=new ArrayList<String[]>();
-        //jashan
+        // gets all the descriptions
         ArrayList<String> colC = dbAccess.getColC();
 
         for (int i = 0; i < colC.size(); i++) {
@@ -102,6 +102,27 @@ public class metric {
         }
 
         return data;
+    }
+
+    private boolean containsIgnoreCase(String src, String what) {
+        final int length = what.length();
+        if (length == 0)
+            return true; // Empty string is contained
+
+        final char firstLo = Character.toLowerCase(what.charAt(0));
+        final char firstUp = Character.toUpperCase(what.charAt(0));
+
+        for (int i = src.length() - length; i >= 0; i--) {
+            // Quick check before calling the more expensive regionMatches() method:
+            final char ch = src.charAt(i);
+            if (ch != firstLo && ch != firstUp)
+                continue;
+
+            if (src.regionMatches(true, i, what, 0, length))
+                return true;
+        }
+
+        return false;
     }
 
     //metric: request maintainer for the user interface
@@ -124,27 +145,6 @@ public class metric {
             print+="\n";
         }
         System.out.println(print);
-    }
-
-    public boolean containsIgnoreCase(String src, String what) {
-        final int length = what.length();
-        if (length == 0)
-            return true; // Empty string is contained
-
-        final char firstLo = Character.toLowerCase(what.charAt(0));
-        final char firstUp = Character.toUpperCase(what.charAt(0));
-
-        for (int i = src.length() - length; i >= 0; i--) {
-            // Quick check before calling the more expensive regionMatches() method:
-            final char ch = src.charAt(i);
-            if (ch != firstLo && ch != firstUp)
-                continue;
-
-            if (src.regionMatches(true, i, what, 0, length))
-                return true;
-        }
-
-        return false;
     }
 
 }
